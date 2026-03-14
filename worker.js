9a5777fb2a0ce8dc46052a6ad245d44315c0ff41
Nicholas Mendes <mendes.nik@hotmail.com>
2026-03-14 15:30:45 -0300

    ajustando o worker

---
+++ worker.js
@@ -0,0 +1,58 @@
+export default {
+  async fetch(request, env) {
+
+    const corsHeaders = {
+      "Access-Control-Allow-Origin": "*",
+      "Access-Control-Allow-Methods": "GET, POST, OPTIONS",
+      "Access-Control-Allow-Headers": "Content-Type"
+    };
+
+    // Preflight
+    if (request.method === "OPTIONS") {
+      return new Response(null, {
+        status: 204,
+        headers: corsHeaders
+      });
+    }
+
+    try {
+
+      const body = await request.json();
+
+      const response = await fetch(
+        "https://api-sandbox.asaas.com/v3/checkouts",
+        {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+            "access_token": env.ASAAS_TOKEN
+          },
+          body: JSON.stringify(body)
+        }
+      );
+
+      const data = await response.text();
+
+      return new Response(data, {
+        status: response.status,
+        headers: {
+          ...corsHeaders,
+          "Content-Type": "application/json"
+        }
+      });
+
+    } catch (error) {
+
+      return new Response(JSON.stringify({
+        error: error.message
+      }), {
+        status: 500,
+        headers: {
+          ...corsHeaders,
+          "Content-Type": "application/json"
+        }
+      });
+
+    }
+  }
+};
